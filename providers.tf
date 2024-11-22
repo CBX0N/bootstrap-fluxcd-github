288b575e5ade092bd0ee3374f83d45ac41949a0a
Charlie Boon <88788571+CBX0N@users.noreply.github.com>
2024-11-22 13:10:55 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,3 +11,21 @@ terraform {
     }
   }
 }
+
+provider "flux" {
+  kubernetes = {
+    config_path = local_file.kubeconfig.filename
+  }
+  git = {
+    url = "https://github.com/${var.github_org}/${var.github_repository}.git"
+    http = {
+      username = "git"
+      password = var.github_token
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github_org
+  token = var.github_token
+}
